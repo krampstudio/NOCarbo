85273458dd0fee652dc06f02e9b5ee87d5d1580f
Bertrand Chevrier <bertrand@elap.(none)>
2011-04-10 23:15:03 +0200

    Adding common servlet, and token generator

---
+++ src/com/krampstudio/nopas/TokenGenerator.java
@@ -0,0 +1,38 @@
+package com.krampstudio.nopas;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+public class TokenGenerator {
+
+	public static String generateToken(HttpServletRequest request){
+		String hash = null;
+		
+		Random r = new Random();
+		Double d = new Double(r.nextGaussian());
+		String clear = d.toString();
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			byte[] digest = md.digest( clear.getBytes() );
+			hash = bytes2String(digest);
+			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return hash;
+	}
+	
+	private static String bytes2String(byte[] bytes) {
+	        StringBuilder string = new StringBuilder();
+	        for (byte b: bytes) {
+	                String hexString = Integer.toHexString(0x00FF & b);
+	                string.append(hexString.length() == 1 ? "0" + hexString : hexString);
+	        }
+	        return string.toString();
+	}
+
+}
